30e0ab3b32718ae0ab9324e0bdb30d2bd1d55ca7
dev-karani <edwin.k.kithinji@gmail.com>
2025-09-09 16:49:12 +0300

    feat: added spline object

---
+++ src/components/pieces/spline.jsx
@@ -0,0 +1,11 @@
+// import Spline from "@splinetool/react-spline";
+
+// const SplineLocal = () => {
+//   return (
+//     <div className="w-full h-[600px]">
+//       <Spline scene="/scene.splinecode" />
+//     </div>
+//   );
+// };
+
+// export default SplineLocal;
