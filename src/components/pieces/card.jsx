2d46af57601e595c0a4592b4c39aabd9ec363e77
dev-karani <edwin.k.kithinji@gmail.com>
2025-09-09 20:52:20 +0300

    update:highlights

--- src/components/pieces/card.jsx
+++ src/components/pieces/card.jsx
@@ -6,14 +6,14 @@ import Button from "../tokens/atoms/button";
 const Card = ({ image, title, description, className = "", ...props }) => {
   return (
     <div
-      className={`h-aspect-[16/9] w-full overflow-hidden shadow-sm ${className}`}
+      className={`h-aspect-[16/9]  overflow-hidden shadow-sm ${className}`}
       {...props}
     >
-      <div className="flex-1">
+      <div className="flex-1 aspect-[16/9] w-full overflow-hidden bg-gray-100">
         <img
           src={image}
           alt={title}
-          className=""
+          className="w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-105"
         />
       </div>
 
