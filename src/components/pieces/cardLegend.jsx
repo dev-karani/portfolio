8237b21fea2da6c0e7a22389bbb9330bd80c22a4
dev-karani <edwin.k.kithinji@gmail.com>
2025-09-09 20:41:51 +0300

    update:legend text

--- src/components/pieces/cardLegend.jsx
+++ src/components/pieces/cardLegend.jsx
@@ -20,13 +20,13 @@ const CardLegend = ({ image, title, description, tags, date, onSelect, to, class
       </div>
 
       <div className="p-6 flex flex-col gap-2">
-        <Text className="text-small font-bold">{title}</Text>
-        <Text className="text-sm text-gray-600 line-clamp-3">{description}</Text>
+        <Text className="text-xl font-bold">{title}</Text>
+        <Text className=" text-gray-600 line-clamp-3">{description}</Text>
 
         {tags && (
           <div className="flex gap-2 flex-wrap mt-2">
             {tags.map((tag, i) => (
-              <span key={i} className="bg-green-200 rounded-full px-2 py-1 text-xs">
+              <span key={i} className="bg-green-200 rounded-full px-2 py-1 ">
                 {tag}
               </span>
             ))}
--- src/components/sections/main.jsx
+++ src/components/sections/main.jsx
@@ -31,7 +31,7 @@ const Main = ({ title, description, projects }) => {
         <div className="grid grid-cols-12">
           <div className="col-start-1 col-end-13">
             {/* Title + description */}
-            <SectionTitle>{title}</SectionTitle>
+            <SectionTitle className="4xl">{title}</SectionTitle>
             <Text className="max-w-xl">{description}</Text>
 
             {/* Only render if projects exist */}
