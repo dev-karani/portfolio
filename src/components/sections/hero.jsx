08bee8fbc0386323d82d7e8e3b15e2d084ab9646
dev-karani <edwin.k.kithinji@gmail.com>
2025-09-09 19:00:27 +0300

    update:text width 72 hero

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -25,7 +25,7 @@ const Hero = () =>{
                             </div>
                             <div className="flex gap-32">
                                 <Text className="w-62">This is my documentation of what I’ve learned and built so far.</Text>
-                                <Text className="w-72">I love breaking down complex concepts into visual analogies and interactive stories—so be sure to check out my guides and labs in Legends.</Text>
+                                <Text className="w-80">I love breaking down complex concepts into visual analogies and interactive stories—so be sure to check out my guides and labs in Legends.</Text>
                             </div>
                     </div>
                 </div>
