d05fbbff90ed1f552ff64f5240c9686593b4e58a
dev-karani <edwin.k.kithinji@gmail.com>
2025-09-25 08:02:10 +0300

    feat: packetTracer writeup

---
+++ src/modals/ciscoPacetTracer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import SectionTitle from '../components/tokens/atoms/sectionTitle'
+
+export default function packetTracer(){
+    <div className='space-y-4'>
+        <SectionTitle>Using Cisco packet Tracer</SectionTitle>
+    </div>
+}
--- src/pages/legend/networking.jsx
+++ src/pages/legend/networking.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Text from '../../components/tokens/atoms/text';
 import Main from '../../components/sections/main';
+import packetTracer from '../../modals/ciscoPacetTracer';
 
 const Networking =() => {
     return (
@@ -9,11 +10,12 @@ const Networking =() => {
             description="Below are writeups and projects I've done through resources like Let's Defend, TryHackMe, and my home labs..."
             projects={[
               {
-                image: "/mlue.png",
-                title: "Blockchain Hackathon",
-                description: "AI hackathon was a blend to create a digital recording tool for African heritage",
-                tags: ["project", "networking"],
-                date: "16 April 2025",
+                image: "/packetTracer.png",
+                title: "Cisco PacketTracer 101",
+                description: "Through lessons and assignments, I learn how to use packetTracer to simulate networks",
+                tags: ["writeup", "networking", "StartingPoint", "Cisco"],
+                date: "25 September 2025",
+                modalComponent: packetTracer, // 🔹 This will render inside Modal
               },
               {
                 image: "/mlue.png",
